b83d75d20eb67c6e98ba2851dda109967cc493be
Adam Ananda Putra <adamanandaputra@gmail.com>
2026-04-04 14:10:26 +0700

    feat: git add cart length to navbar cart icon

--- src/components/navbar/cart-button.tsx
+++ src/components/navbar/cart-button.tsx
@@ -1,9 +1,17 @@
+"use client";
+
 import { hoverEffect, tapEffect } from "@/utils/motion-effects";
 import { motion } from "motion/react";
-import { ShoppingCart } from "lucide-react";
+import { ShoppingCart, Loader2 } from "lucide-react";
+import { useQuery } from "@tanstack/react-query";
+import { CartItemRes } from "@/types/carts-interface";
+import { cartApi } from "@/services/cart-api";
 
 export default function CartButton() {
-  const dummyCartQty = 0;
+  const { data: carts, isPending } = useQuery<CartItemRes[]>({
+    queryKey: ["carts"],
+    queryFn: cartApi.getCart,
+  });
 
   return (
     <motion.div
@@ -13,7 +21,11 @@ export default function CartButton() {
     >
       <ShoppingCart className="size-c-7 md:size-c-9 lg:size-c-5 cursor-pointer hover:text-pink-500 active:text-pink-600" />
       <span className="absolute -top-c-3 -right-c-3 lg:-top-c-3 lg:left-c-3 bg-pink-500 text-c-3 size-c-6 rounded-full grid place-items-center text-white">
-        {dummyCartQty}
+        {isPending ? (
+          <Loader2 className="w-3/4 h-3/4 animate-spin" />
+        ) : (
+          carts?.length || 0
+        )}
       </span>
     </motion.div>
   );
